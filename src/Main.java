40035d092e3823c5e0373cd7f7bea9fe56c80d4a
헥스/박해승 <2001phs92@gmail.com>
2023-11-04 10:24:08 +0900

    make main frame

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
-import java.sql.*;
-import java.io.*;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import Frame.*;
+import javax.swing.*;
 
 public class Main {
     public static void main(String args[]) {
-        DAO dao = DAO.sharedInstance();
-
-        List<EMPLOYEE> list = dao.getList();
-        for (EMPLOYEE e : list) {
-            System.out.println(Arrays.toString(e.toArray()));
-        }
+        SwingUtilities.invokeLater(() -> {
+            JDBCFrame gui = new JDBCFrame();
+            gui.repaint();
+        });
     }
 }
